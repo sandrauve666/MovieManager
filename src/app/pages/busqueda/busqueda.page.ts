8fcc1041b2b9589046db0331742537e6bb0503eb
sandrauve <sandrauve666@gmail.com>
2024-09-04 20:32:30 +0200

    Update busqueda.page.ts

--- src/app/pages/busqueda/busqueda.page.ts
+++ src/app/pages/busqueda/busqueda.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from "ionicons";
 
 @Component({
   selector: 'app-busqueda',
